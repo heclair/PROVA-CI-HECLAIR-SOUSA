58f62d7eab9109e29709008d981a98f9c7920219
heclair <heclair@hotmail.com>
2025-11-10 21:02:43 -0300

    terceiro commit

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,10 +1,11 @@
 /** @type {import('jest').Config} */
 module.exports = {
   testEnvironment: 'node',
-  preset: 'ts-jest',
+  transform: { '^.+\\.ts$': ['ts-jest', { tsconfig: 'tsconfig.json' }] },
   collectCoverage: true,
   coverageDirectory: 'coverage',
   collectCoverageFrom: ['**/*.ts', '!**/*.d.ts', '!dist/**', '!**/*.test.ts'],
   testMatch: ['**/?(*.)+(spec|test).ts'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
   verbose: true
 };
